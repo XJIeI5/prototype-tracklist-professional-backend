8accee48e3526108252e535fc738ab4ea4662d79
XJIEI5 <egorchernov37@yandex.ru>
2024-04-02 15:03:57 +0300

    try implement login

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import flask
+from sqlalchemy import exc
 from data import db_session
+from data import users
 
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return flask.redirect("/login")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if flask.request.method == "GET":
+        return flask.render_template("login.html")
+    
+    email, phone = flask.request.form["email"], flask.request.form["phone"]
+    sess = db_session.create_session()
+
+    try:
+        user = sess.query(users.User).filter(
+            (users.User.email == email) & (users.User.phone == int(phone))
+        ).one()
+    except exc.NoResultFound:
+        return "NO USER FOUND"
+    return "OK"
 
 
 def main():
